2ca51878a2a1a4915da15f7d6ef446ea33208300
tobyatgithub <toby.fangyuan@gmail.com>
2024-02-09 15:14:00 +0800

    Initial Commit

---
+++ 20-20-20/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  20-20-20
+//
+//  Created by Toby Huang on 2/9/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 20-20-20/_0_20_20App.swift
@@ -0,0 +1,17 @@
+//
+//  _0_20_20App.swift
+//  20-20-20
+//
+//  Created by Toby Huang on 2/9/24.
+//
+
+import SwiftUI
+
+@main
+struct _0_20_20App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
